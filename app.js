3e867e5f1ce27cb328eed316bda6750c9815936f
Val-entine92 <111467473+Val-entine92@users.noreply.github.com>
2024-02-11 01:55:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 
 function accepted() {
     var response = prompt("I LOVE YOU");
-    if (response !== null) {
-        response = response.toLowerCase();
-        if (response === "i love you too") {
-            alert("Great choice!");
-        } else {
-            alert("Say it BACK");
-            var another_response=prompt("I LOVE YOU");
-            if (another_response!== "i love you"){
-                prompt("SAY IT BACKKKK")
-            }
-            else {
-                alert("Thank you")
+    if (response === "i love you too" || "i love you") {
+        alert("Great choice!");
         }
-    }
-}
+    else {
+        alert("Say it BACK");
+        var another_response=prompt("I LOVE YOU");
+        if (another_response!== "i love you"){
+            var last_chance=prompt("SAY IT BACKKKK")
+            }
+        else {
+            alert("Thank you , have a nice day")
+            }
+      }
 }
