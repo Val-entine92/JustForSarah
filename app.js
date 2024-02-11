dfce440c922849f09b8fd17d53e338b9233331dc
Val-entine92 <111467473+Val-entine92@users.noreply.github.com>
2024-02-11 02:16:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 function accepted() {
     var response = prompt("I LOVE YOU");
-    if (response === "i love you too" || "i love you") {
-        alert("Great choice!");
+    if (response === "i love you too") {
+        alert("I love you . Have a nice day!");
         }
     else {
-        alert("Say it BACK");
+        alert("Say it back Please");
         var another_response=prompt("I LOVE YOU");
         if (another_response!== "i love you"){
             var last_chance=prompt("SAY IT BACKKKK")
